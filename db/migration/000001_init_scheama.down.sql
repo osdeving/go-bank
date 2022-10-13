49cb551bfb1ed42f2b282c249eae5322bccf4d30
Willams Sousa <osdeving@gmail.com>
2022-10-12 23:59:09 -0300

    refact

--- db/migration/000001_init_scheama.down.sql
+++ db/migration/000001_init_scheama.down.sql
@@ -1,4 +1,4 @@
-DROP TABLE IF EXISTS entries cascade;
-DROP TABLE IF EXISTS transfers cascade;
-DROP TABLE IF EXISTS accounts cascade;
+DROP TABLE IF EXISTS entries;
+DROP TABLE IF EXISTS transfers;
+DROP TABLE IF EXISTS accounts;
 
---
+++ db/sqlc/account_test.go
@@ -0,0 +1,55 @@
+package db
+
+import (
+	"context"
+	"testing"
+
+	"github.com/osdeving/go-bank/util"
+	"github.com/stretchr/testify/require"
+)
+
+func createRandomAccount(t *testing.T) Account {
+	user := createRandomUser(t)
+
+	arg := CreateAccountParams{
+		Owner:    user.Username,
+		Balance:  util.RandomMoney(),
+		Currency: util.RandomCurrency(),
+	}
+
+	account, err := testQueries.CreateAccount(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, account)
+
+	require.Equal(t, arg.Owner, account.Owner)
+	require.Equal(t, arg.Balance, account.Balance)
+	require.Equal(t, arg.Currency, account.Currency)
+
+	require.NotZero(t, account.ID)
+	require.NotZero(t, account.CreatedAt)
+
+	return account
+}
+
+func TestCreateAccount(t *testing.T) {
+	createRandomAccount(t)
+}
+
+// func TestCreaAccount(t *testing.T) {
+// 	arg := CreateAccountParams{
+// 		Owner: util.RandomOwner(),
+// 		Balance: util.RandomMoney(),
+// 		Currency: util.RandomCurrency(),
+// 	}
+
+
+// 	account, err := testQueries.CreateAccount(context.Background(), arg)
+
+// 	require.NoError(t, err)
+// 	require.NotEmpty(t, account)
+// 	require.Equal(t, arg.Owner, account.Owner)
+// 	require.Equal(t, arg.Balance, account.Balance)
+// 	require.Equal(t, arg.Currency, account.Currency)
+// 	require.NotZero(t, account.ID)
+// 	require.NotZero(t, account.CreatedAt)
+// }
